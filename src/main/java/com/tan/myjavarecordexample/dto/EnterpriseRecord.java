cfb50b0e880e84a75ec55b46f1ec4b32f151cd2a
Evren Tan <evrentan@gmail.com>
2021-10-25 00:34:52 +0300

    java validation dependency is added
    java validation dependency is added

--- src/main/java/com/tan/myjavarecordexample/dto/EnterpriseRecord.java
+++ src/main/java/com/tan/myjavarecordexample/dto/EnterpriseRecord.java
@@ -1,4 +1,8 @@
 package com.tan.myjavarecordexample.dto;
 
-public record EnterpriseRecord(String id, String name, String address) {
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotNull;
+
+public record EnterpriseRecord(String id, @NotNull String name, @Length(min = 1, max = 255) String address) {
 }
