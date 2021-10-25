5ac3bfa21634d9b5ce1fa6932445635da96ce9be
Evren Tan <evrentan@gmail.com>
2021-10-25 11:20:48 +0300

    Serializable is implemented in EnterpriseRecord class
    Serializable is implemented in EnterpriseRecord class

--- src/main/java/com/tan/myjavarecordexample/dto/EnterpriseRecord.java
+++ src/main/java/com/tan/myjavarecordexample/dto/EnterpriseRecord.java
@@ -3,6 +3,7 @@ package com.tan.myjavarecordexample.dto;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotNull;
+import java.io.Serializable;
 
-public record EnterpriseRecord(String id, @NotNull String name, @NotNull @Length(min = 2, max = 255) String address) {
+public record EnterpriseRecord(String id, @NotNull String name, @NotNull @Length(min = 2, max = 255) String address) implements Serializable {
 }
